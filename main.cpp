36c3cbe2c0b3383968730b11b9c2a5a09aab497c
AndyGiles <63230398+AndyGiles@users.noreply.github.com>
2020-07-15 20:15:32 -0700

    Update and rename nthprime.cpp to main.cpp
    Separated the functions from the main program.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <ctime>
 #include <chrono>
-
-// checks if a given integer is prime, returns true if prime and false if not prime
-
-bool isPrime(int num) {
-  if (num % 2 == 0 or num % 3 == 0) {
-    return (num == 2 || num == 3);
-  }
-  for (int i = 6; i < pow(num, 0.5) + 2; i += 6) {
-    if ((num % (i - 1) == 0) || (num % (i + 1) == 0)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-int mainSearch(int n, int found, int testing) {
-  if (n == 1 || n == 2) {
-    return n + 1;
-  }
-  while (found < n) {
-    testing += 6;
-    found += (isPrime(testing - 1) + isPrime(testing + 1));
-  }
-  return testing + (2 * (found == n && isPrime(testing + 1))) - 1;
-}
+#include "header.hpp"
 
 int main() {
   int n;
