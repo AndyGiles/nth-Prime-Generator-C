f1897a61c5343aab210418ab9c797a98bda81a7f
Andy Giles <63230398+AndyGiles@users.noreply.github.com>
2020-07-16 03:06:33 -0700

    Update main.cpp
    Added more streamlined functions outside of main to handle the time rounding, removed this functionality from main itself.

--- main.cpp
+++ main.cpp
@@ -15,17 +15,8 @@ int main() {
   int result = mainSearch(n, found, testing);
 
   std::chrono::system_clock::time_point end = std::chrono::system_clock::now(); // ends the timer
-  std::chrono::duration<double, std::micro> mis = end - start; // creates a variable that stores the total time in microseconds
   std::cout << result << "\n\n"; // prints the nth prime
-  if (mis < std::chrono::milliseconds(1)) {
-    std::cout << mis.count() << " microseconds\n";
-  }
-  else if (mis < std::chrono::seconds(1)) {
-    std::chrono::duration<double, std::milli> ms = mis; // converts to milliseconds if the total time is between 1 millisecond and 1 second
-    std::cout << ms.count() << " milliseconds\n";
-  }
-  else {
-    std::chrono::duration<double> s = mis; // converts to seconds if the total time is at least 1 second
-    std::cout << s.count() << " seconds\n";
-  }
+  std::chrono::duration<double> total = end - start; // creates a variable that stores the total time in microseconds
+  double ftotal = total.count();
+  std::cout << printTime(ftotal);
 }
